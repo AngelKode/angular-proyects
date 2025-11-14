e8faf79a6bcf01b8cf2bdab0b6c39a2b1f375ef0
AngelKode <200angel@live.com.mx>
2025-11-14 11:20:15 -0600

    Add: Class and composition

--- v20/intro-ts/src/topics/classes.ts
+++ v20/intro-ts/src/topics/classes.ts
@@ -1,11 +1,34 @@
-interface Owner{
-    name : string,
-    age : number
-}
-class Animal{
-    constructor (
-        public name : string,
-        private uniqueId:number,
-        private owner : Owner | undefined
+class DogService{
+    constructor(
+        private name : string,
+        private isCertificated : boolean,
+        private dogsToWork:Dog[]
     ){}
+
+    getDogsToWork():Dog[]{
+        return this.dogsToWork;
+    }
+
+    getCertificationOk():boolean{
+        return this.isCertificated;
+    }
+
+    getName():string{
+        return this.name;
+    }
+}
+
+class Dog{
+    constructor(
+        private name : string,
+        private dogService : DogService
+    ){}
+
+    bark():void{
+        console.log(`${this.name}: Woof Woof`);
+    }
+
+    getMyOwner():string{
+        return this.dogService.getName();
+    }
 }
